b0d13bed43b592222190f1f7f3eddf82038bf593
Tyler Sampson <tyler@aviseo.co.za>
2024-02-26 11:04:02 +0200

    Fix missing import

--- base_tier_validation_signature/models/tier_review.py
+++ base_tier_validation_signature/models/tier_review.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, models, fields
 
 class TierReview(models.Model):
     _inherit = "tier.review"
