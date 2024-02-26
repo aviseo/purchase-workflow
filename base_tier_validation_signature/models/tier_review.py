0778cd16aeeb583c79a1388c8cfdd03d7561f339
Tyler Sampson <tyler@aviseo.co.za>
2024-02-26 12:11:48 +0200

    Base64 images

--- base_tier_validation_signature/models/tier_review.py
+++ base_tier_validation_signature/models/tier_review.py
@@ -2,9 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, fields
+import base64
 
 class TierReview(models.Model):
     _inherit = "tier.review"
 
     signature = fields.Image()
+
+    signature_b64 = fields.Char(compute="_compute_signature_b64")
+
+    @api.depends("signature")
+    def _compute_signature_b64(self):
+        for rec in self:
+            rec.signature_b64 = rec.signature and base64.b64encode(rec.signature).decode("utf-8") or False
     
